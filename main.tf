3210a7567aeda618393dd33f2b0e3b22f526ba69
cosmos-explorer <lian.yang.work@gmail.com>
2022-04-02 23:42:41 +0200

    removed duplicate of terraform block

--- main.tf
+++ main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
-  required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "featureflags"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
-}
-
 provider "aws" {
   region = "cn-northwest-1"
 }
--- versions.tf
+++ versions.tf
@@ -1,5 +1,6 @@
+/*
 terraform {
-    /*
+    
     cloud {
         organization = "tuto-jj"
 
@@ -7,7 +8,7 @@ terraform {
             name = "learn-terraform-cloud"
         }
     }
-    */
+    
     required_providers {
       aws = {
           source = "hashicorp/aws"
@@ -16,4 +17,26 @@ terraform {
     }
 
     required_version = ">= 1.1.0"
+}
+*/
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.28.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "featureflags"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
 }
